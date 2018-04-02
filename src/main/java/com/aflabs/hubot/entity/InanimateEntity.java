eb45e14c4d02be9c7dffc31c6fe803af5514dce5
shastrijs <shastri.jagarlapudi@gmail.com>
2018-04-02 13:40:48 -0400

    First commit

---
+++ src/main/java/com/aflabs/hubot/entity/InanimateEntity.java
@@ -0,0 +1,14 @@
+package com.aflabs.hubot.entity;
+
+public class InanimateEntity extends SubjectEntity {
+
+	public InanimateEntity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
